837dd0cbea1ac2a55e589ce5971d8a9986f6fa49
Naga2160 <reddykasi46@gmail.com>
2024-10-06 17:31:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ region="ap-south-1"
 resource "aws_instance" "one"{
 ami="ami-078264b8ba71bc45e"
 instance_type="t2.micro"
-key_name="docker"
+key_name="dev4pm"
 vpc_security_group_ids=["sg-049e104c5f8955ef5"]
 
 
