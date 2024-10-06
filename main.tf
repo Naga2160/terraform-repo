f219dd707d6da061c98854ace071ead03b3ddf9a
Naga2160 <reddykasi46@gmail.com>
2024-10-06 17:21:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,6 +4,9 @@ region="ap-south-1"
 resource "aws_instance" "one"{
 ami="ami-078264b8ba71bc45e"
 instance_type="t2.micro"
+key_name="docker"
+vpc_security_group_ids=["sg-049e104c5f8955ef5"]
+
 
 tags={
 Name="naga"
